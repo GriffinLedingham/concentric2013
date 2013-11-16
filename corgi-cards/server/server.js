c14a81b093a0813cd33f662e8fb1167669a80ff2
GriffinLedingham <gcledingham@gmail.com>
2013-11-15 19:41:37 -0800

    [griff] pushing initial server and index file which communicates with it in room 1, also set gitignore for nodemodules

--- corgi-cards/server/server.js
+++ corgi-cards/server/server.js
@@ -1,12 +1,35 @@
 var fs = require('fs')
 var app = require('http').createServer(handler);
-app.listen(8080);
+app.listen(8142);
 
 var io = require('socket.io').listen(app);		  
 io.set('log level', 0);
 
 io.sockets.on('connection', function (socket) {
-	socket.on('',function(){
+	console.log('connected');
+	socket.room;
+	socket.guid;
 
+	socket.on('auth',function(guid_in){
+		socket.guid = guid_in;
+	});
+
+	socket.on('join_room', function(room){
+		socket.room = room;
+		socket.join(room);
+		socket.broadcast.emit('message',{guid:socket.guid,data:socket.guid+' joined the room'});
 	});
 });
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+ 
+    res.writeHead(200);
+    res.end(data);
+  });
+}
